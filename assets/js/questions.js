716579ffef6a3f36802abffbb6d50905475cffaf
Carlito Algarin <19lito96@gmail.com>
2020-09-15 16:50:59 -0400

    added to the questions javascript file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,11 +1,13 @@
 var questions = [
 {
     title: "Example Question 1:",
-    choices: ["","","",]
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
     answer:
+},
+{
+title: "Example Question 2:",
+choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+answer: "answer from choices"
 }
 
-
-
-
-]
+];
