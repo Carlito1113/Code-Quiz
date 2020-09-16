78f3166d301990879f6fa9d63e32bd92b7c687c5
Carlito Algarin <19lito96@gmail.com>
2020-09-16 16:41:55 -0400

    added in form elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,121 +31,135 @@ var resultDisplay = document.createElement("p");
 var score = 0;
 var highscores = [];
 
-
 // event listener to start the quiz
-startBtn.addEventListener("click", startQuiz)
+startBtn.addEventListener("click", startQuiz);
 // call function to show the opening page
 openingPage();
 
 // Functions
 
 // call for opening page
-    onload();
-    init();
+onload();
+init();
 
 startBtn.onclick = startQuiz;
 
 // Load page contents
 function onLoad() {
-    mainDisplay.textContent = "Click to start!"
-    startBtn.textContent = "Start";
-    displayQuestionsEl.append(mainDisplay, startBtn);
-    hs.innerHTML = "Highscores";
-    timerEl.prepend(hs);
-
+  mainDisplay.textContent = "Click to start!";
+  startBtn.textContent = "Start";
+  displayQuestionsEl.append(mainDisplay, startBtn);
+  hs.innerHTML = "Highscores";
+  timerEl.prepend(hs);
 }
 
 function openingPage() {
-    mainDisplay.textContent = "Press the button to start"
-    startBtn.textContent = "Start"
-    displayQuestionsEl.append(mainDisplay, startBtn);
+  mainDisplay.textContent = "Press the button to start";
+  startBtn.textContent = "Start";
+  displayQuestionsEl.append(mainDisplay, startBtn);
 }
 
 function startQuiz() {
-    showTimer();
-    nextQuestion();
-
+  showTimer();
+  nextQuestion();
 }
 // Timer
 function showTimer() {
+  timerEl.textContent = "Time remaining: " + timer;
 
-    timerEl.textContent = "Time remaining: " + timer;
-
-    var questionTimer = setInterval(function(){
-        // decrease timer by one
-        timer--
-        // display timer to screen
-        timerEl.textcontent = "Time remaining: " + timer;
-        if(timer <= 0){
-            clearInterval(questionTimer);
-        }
-
-    }, 1000)
-
+  var questionTimer = setInterval(function () {
+    // decrease timer by one
+    timer--;
+    // display timer to screen
+    timerEl.textcontent = "Time remaining: " + timer;
+    if (timer <= 0) {
+      clearInterval(questionTimer);
+    }
+  }, 1000);
 }
 
 // Displays the next question
 function nextQuestion() {
-    // Stores question index
-    var currentQuestion = questions[index];
-    // console test
-    console.log(currentQuestion);
-    // Empty question container
-    displayQuestionEl.textContent = "";
-    // Adds current question into the container
-    mainDisplay.textContent = currentQuestion.title;
-    // Append the text to show
-    displayQuestionEl.append(mainDisplay);
-    // div to wrap choices
-    var choicesContainer = document.createElement("div");
-    choicesContainer.classList.add("buttonDiv");
-    // loop to change answer
-    for (let i = 0; i < currentQuestion.choices.length; i++) {
-        // Button for choices
-        var choiceBtn = document.createElement("button");
-        choiceBtn.textContent = currentQuestion.choices[choice];
-        choiceBtn.classList.add("answer-btn");
-        choicesContainer.append(choiceBtn);
-        // Check for answer
-        choiceBtn.onclick = checkAnswer;
-        choicesContainer.append(choiceBtn);
-    }
-    displayQuestionsEl.append(choicesContainer);
+  // Stores question index
+  var currentQuestion = questions[index];
+  // console test
+  console.log(currentQuestion);
+  // Empty question container
+  displayQuestionEl.textContent = "";
+  // Adds current question into the container
+  mainDisplay.textContent = currentQuestion.title;
+  // Append the text to show
+  displayQuestionEl.append(mainDisplay);
+  // div to wrap choices
+  var choicesContainer = document.createElement("div");
+  choicesContainer.classList.add("buttonDiv");
+  // loop to change answer
+  for (let i = 0; i < currentQuestion.choices.length; i++) {
+    // Button for choices
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = currentQuestion.choices[choice];
+    choiceBtn.classList.add("answer-btn");
+    choicesContainer.append(choiceBtn);
+    // Check for answer
+    choiceBtn.onclick = checkAnswer;
+    choicesContainer.append(choiceBtn);
+  }
+  displayQuestionsEl.append(choicesContainer);
 }
 
 // function to check the answer
 function checkAnswer(event) {
+  resultDisplay.setAttribute("class", "result");
+  var responseText = event.target.textContent;
+  console.log(responseText);
 
-    resultDisplay.setAttribute("class", "result")
-    var responseText = event.target.textContent;
-    console.log(responseText);
-
-    if (respsonseText === questions[index].answer) {
-        index++
-        console.log("Correct");
-        if (timer <= 0 || index == questions.length){
-            gameOver();
-        } else{
-            nextQuestion();
-            resultDisplay.innerHTML = "Correct!";
-            displayQuestionsEl.appendChild(resultDisplay);
-        }
-
-
+  if (respsonseText === questions[index].answer) {
+    index++;
+    console.log("Correct");
+    if (timer <= 0 || index == questions.length) {
+      gameOver();
     } else {
-        timer = timer-10;
-        index++
-        console.log("Incorrect");
-        if (timer <= 0 || index == quesitons.length){
-            gameOver();
-        } else {
-            nextQuestion();
-            resultDisplay.innerHTML = "Wrong!";
-            displayQuestionsEl.appendChild(resultDisplay);
-        }
+      nextQuestion();
+      resultDisplay.innerHTML = "Correct!";
+      displayQuestionsEl.appendChild(resultDisplay);
     }
+  } else {
+    timer = timer - 10;
+    index++;
+    console.log("Incorrect");
+    if (timer <= 0 || index == quesitons.length) {
+      gameOver();
+    } else {
+      nextQuestion();
+      resultDisplay.innerHTML = "Wrong!";
+      displayQuestionsEl.appendChild(resultDisplay);
+    }
+  }
 }
 
-
-
-
+function gameOver() {
+  if (time < 0) {
+    score = 0;
+  } else {
+    score = timer;
+  }
+// Form
+var bottomDiv = document.querySelector(".buttonDiv");
+timerEl.getElementsByClassName.display = "none";
+resultDisplay.style.display = "none";
+bottomDiv.innerHTML = "";
+mainDisplay.textContent = "Your score is " + score + "!";
+form.setAttribute("type", "text");
+form.setAttribute("name", "Name");
+form.setAttribute("placeholder", "Enter your name");
+sButton.setAttribute("type", "submit");
+sButton.setAttribute("value", "Submit");
+sButton.classList.add("endButton");
+sButton.style.margin = "10px";
+bottomDiv.appendChild(initialForm);
+initialForm.appendChild(form);
+initialForm.appendChild(sButton);
+goBack.classList.add("endButton");
+goBack.innerHTML = "Go Back";
+bottomDiv.appendChild(goBack);
+}
